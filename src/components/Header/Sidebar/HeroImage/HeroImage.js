5654dda0709ac9986443ca02722da7d30afb35df
Mohammad Sabir Khan <mohsabkha@gmail.com>
2022-01-09 14:19:30 +0500

    animated landing page charactere

--- src/components/Header/Sidebar/HeroImage/HeroImage.js
+++ src/components/Header/Sidebar/HeroImage/HeroImage.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
-import Image from './../../../../images/boxyV2Template.png';
-
 const HeroImage = () => {
     return (
         <div className='hero-image'>
-            <div className='hero-image_inner'>
-                    <img  src={Image} alt='mohammad sabir khan'></img>
-            </div>
-           
+            <div className='hero-image_inner'></div>
         </div>
     )
 }
