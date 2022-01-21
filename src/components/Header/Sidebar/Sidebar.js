c7f94e607310ed49553c75c7d72ee0550c8a40c3
Mohammad Sabir Khan <mohsabkha@gmail.com>
2022-01-21 10:28:47 +0500

    added basic sound

--- src/components/Header/Sidebar/Sidebar.js
+++ src/components/Header/Sidebar/Sidebar.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import useSound from 'use-sound';
 
-import HeroImage from './HeroImage/HeroImage'
+import Beep from './../../../sounds/ui_sound.mp3';
+import HeroImage from './HeroImage/HeroImage';
 
 const Sidebar = () => {
+    const [isClicked] = useSound(Beep, {volume:.25})
     return (
         <div className='sidebar-styles'>
             <HeroImage />
-            <a className='sidebar-link-styles' href='#AboutMe'>Click Here!</a>
+            <a className='sidebar-link-styles' onClick={isClicked} href='#AboutMe'>Click Here!</a>
         </div>
     )
 }
